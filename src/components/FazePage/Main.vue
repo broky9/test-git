da0b18f933de9d53d850067b7dfde8ab267090b0
broky9 <1048082086@qq.com>
2025-04-14 15:55:52 +0800

    20250414_1

--- src/components/FazePage/Main.vue
+++ src/components/FazePage/Main.vue
@@ -1,12 +1,17 @@
 <template>
-    <Elige @change-name=changeName($event)></Elige>
+    {{ name }} __________ {{ age }}
+    <Elige @change-name=changeName($event) :nickName="eligeChineseNickName"></Elige>
+    <div>
+        <button @click="changeAge()">修改age</button>
+    </div>
 
     real name {{ proName }}
 
-    <div>
+    <div style="display: flex; align-items: center; flex-wrap: wrap; justify-content: space-between;">
         <template v-for=" (p,index) in teamPicture" :key="index">
-            <img :src="getPicFile(p.src)" v-if="index==0" style="position: sticky;top:0">
-            <img :src="getPicFile(p.src)" v-if="index!=0">
+            <!-- <img :src="getPicFile(p.src)" v-if="index==0" style="position: sticky;top:0">
+            <img :src="getPicFile(p.src)" v-if="index!=0"> -->
+            <img :src="getPicFile(p.src)" style="width: 600px">
         </template>
     </div>
     <template v-for=" p in teamPicture" :key="p.src">
@@ -17,13 +22,20 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, watchEffect } from 'vue'
 import Elige from './team-player/Elige.vue';
 import useUtils from '@/hooks/useUtils';
 
+const eligeChineseNickName = '鸡哥';
+
 const proName = ref('12')
 
+const name = ref('faze')
+
+const age = ref(0)
+
 const { getPicFile } = useUtils();
+
 
 const teamPicture = [
     {src: '../img/faze-clan/broky.jpg'},
@@ -37,5 +49,18 @@ const teamPicture = [
 function changeName ($event) {
     proName.value = $event
 }
+
+watch(name,(newValue,oldValue) => {
+    console.log(newValue,oldValue)
+})
+watch(age,(newValue,oldValue) => {
+    console.log(newValue,oldValue)
+})
+
+function changeAge() {
+    age.value++;
+}
+
+defineExpose({name,age})
 
 </script>
--- src/components/FazePage/team-player/Elige.vue
+++ src/components/FazePage/team-player/Elige.vue
@@ -1,15 +1,25 @@
 <template>
     <h1>name: elige</h1>
+    {{ nickName }}
     <button @click="changeName()"> 修改名字 </button>
+    <button @click="changeFaze($parent)">修改faze</button>
 </template>
 
 <script setup>
+
+defineProps(['nickName'])
 
 const emit = defineEmits(['change-name'])
 
 
 const changeName = () => {
     emit('change-name','elige')
+}
+
+function changeFaze(parent) {
+    console.log(parent.name);
+    parent.name = parent.name.toUpperCase();
+    parent.age++;
 }
     
 </script>
